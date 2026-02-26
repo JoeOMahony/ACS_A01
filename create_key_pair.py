c538414eaafa16a0b04e6514f53e887e6acb1d95
joe <21788075@mail.wit.ie>
2026-02-26 19:39:52 +0000

    Create key pair function Added a create key pair function for EC2/S3 instance  automation

---
+++ create_key_pair.py
@@ -0,0 +1,46 @@
+import time
+
+import boto3
+import datetime
+
+# DOCS
+#
+
+ec2c = boto3.client('ec2')
+
+"""
+Creates a unique RSA key-pair using the EC2 client handle
+=> Uniqueness provided through nanoseconds since Epoch (1/Jan/70)
+   ==> Nanoseconds selected to ensure uniqueness, but also because time.time_ns() returns an int
+=> Docs: https://docs.aws.amazon.com/boto3/latest/reference/services/ec2/client/create_key_pair.html
+=> Key name: acs_a01_jomahony
+
+:return dict describing the key-pair
+"""
+def create_key_pair():
+    key_name = f"acs_a01_jomahony_{time.time_ns()}" # using nanoseconds from Epoch to make each instance key unique
+    response = ec2c.create_key_pair(
+        KeyName=key_name,
+        KeyType='rsa',
+        KeyFormat='pem',
+        TagSpecifications=[
+            {
+                "ResourceType": "key-pair",
+                "Tags": [
+                    {"Key": "CreatedBy", "Value": "JoeOMahony"},
+                    {"Key": "Module", "Value": "AutomatedCloudServices"},
+                    {"Key": "Assignment", "Value": "Assignment01"}
+                ]
+            }
+        ]
+    )
+
+    return response
+
+"""
+Basic test for the create_key_pair() function through creating a key and printing the output
+"""
+def test_create_key_pair():
+    print(create_key_pair())
+
+test_create_key_pair()
